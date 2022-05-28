4bdb45dc17c2bb996e8353c899ffd2aabc036558
ElenaKarseka <105430944+ElenaKarseka@users.noreply.github.com>
2022-05-28 23:37:17 +0300

    MedalIcon Medals

--- AchiveClubServer/Services/Medals/TotalRatingMedals.cs
+++ AchiveClubServer/Services/Medals/TotalRatingMedals.cs
@@ -15,13 +15,39 @@ namespace AchiveClubServer.Services
 
         public List<UserMedal> GetAllMedals()
         {
-            var medals = _userStorage.UserRating.Take(3).Select(u => new UserMedal { Medal = 21, User = u.User.Id }).ToList();
-            medals.AddRange(_userStorage.UserRating.Take(10).Select(u => new UserMedal { Medal = 22, User = u.User.Id }).ToList());
-            medals.AddRange(_userStorage.UserRating.Take(25).Select(u => new UserMedal { Medal = 23, User = u.User.Id }).ToList());
-            medals.AddRange(_userStorage.UserRating.Take(50).Select(u => new UserMedal { Medal = 24, User = u.User.Id }).ToList());
-            medals.AddRange(_userStorage.UserRating.Take(100).Select(u => new UserMedal { Medal = 25, User = u.User.Id }).ToList());
-            medals.AddRange(_userStorage.UserRating.Take(250).Select(u => new UserMedal { Medal = 26, User = u.User.Id }).ToList());
-            return medals;
+            List<UserMedal> medals3 = _userStorage.UserRating.Take(3).Select(u => new UserMedal { Medal = 21, User = u.User.Id }).ToList();
+            List<UserMedal> medals10 =_userStorage.UserRating.Take(10).Select(u => new UserMedal { Medal = 22, User = u.User.Id }).ToList();
+            List<UserMedal> medals25 = _userStorage.UserRating.Take(25).Select(u => new UserMedal { Medal = 23, User = u.User.Id }).ToList();
+            List<UserMedal> medals50 = _userStorage.UserRating.Take(50).Select(u => new UserMedal { Medal = 24, User = u.User.Id }).ToList();
+            List<UserMedal> medals100 =_userStorage.UserRating.Take(100).Select(u => new UserMedal { Medal = 25, User = u.User.Id }).ToList();
+            List<UserMedal> medals250 = _userStorage.UserRating.Take(250).Select(u => new UserMedal { Medal = 26, User = u.User.Id }).ToList();         
+            medals250 = deleteRepeating(medals100, medals250);
+            medals100 = deleteRepeating(medals50, medals100);
+            medals50 = deleteRepeating(medals25, medals50);
+            medals25 = deleteRepeating(medals10, medals25);
+            medals10 = deleteRepeating(medals3, medals10);
+            medals3.AddRange(medals10);
+            medals3.AddRange(medals25);
+            medals3.AddRange(medals50);
+            medals3.AddRange(medals100);
+            medals3.AddRange(medals250);
+            return medals3;
+        }
+
+        private List<UserMedal> deleteRepeating(List<UserMedal> medals1, List<UserMedal> medals2)
+        {
+            for (int j = 0; j < medals1.Count; j++)
+            {
+                for (int i = 0; i < medals2.Count; i++)
+                {
+                    if (medals1[j].User == medals2[i].User)
+                    {
+                        medals2.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            return medals2;
         }
     }
 }
